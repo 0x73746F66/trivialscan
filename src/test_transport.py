116162d610525b00ca50d94f5cdfdc8c360b6e71
chris <chris@trivialsec.com>
2022-05-01 01:16:55 +1000

    v2.3.2

--- src/test_transport.py
+++ src/test_transport.py
@@ -1,36 +1,34 @@
 from trivialscan.transport import Transport
 from trivialscan import constants
 
-HOST = "http2.github.io"
+HOST = "google.com"
+
+TRANSPORT = Transport(HOST)
 
 
-class TestMetadata:
-    _transport = Transport(HOST)
+def test_setup():
+    assert TRANSPORT.connect_least_secure() is True
 
-    def setup(self):
-        if not hasattr(self, "_transport"):
-            self._transport.connect_least_secure()
 
-    def test_host(self):
-        self.setup()
-        assert self._transport.host == HOST
+def test_host():
+    assert TRANSPORT.host == HOST
 
-    def test_port(self):
-        self.setup()
-        assert 443 == self._transport.port
 
-    def test_negotiated_cipher(self):
-        self.setup()
-        assert isinstance(self._transport.negotiated_cipher, str)
+def test_port():
+    assert 443 == TRANSPORT.port
 
-    def test_negotiated_protocol(self):
-        self.setup()
-        assert self._transport.negotiated_protocol in [
-            constants.TLS1_0_LABEL,
-            constants.TLS1_1_LABEL,
-            constants.TLS1_2_LABEL,
-        ]
 
-    def test_sni_support(self):
-        self.setup()
-        assert isinstance(self._transport.sni_support, bool)
+def test_negotiated_cipher():
+    assert isinstance(TRANSPORT.negotiated_cipher, str)
+
+
+def test_negotiated_protocol():
+    assert TRANSPORT.negotiated_protocol in [
+        constants.TLS1_0_LABEL,
+        constants.TLS1_1_LABEL,
+        constants.TLS1_2_LABEL,
+    ]
+
+
+def test_sni_support():
+    assert isinstance(TRANSPORT.sni_support, bool)
