348a2ff3b19ff9fbed9501ee551997a622b50509
chris <chris@trivialsec.com>
2022-06-10 14:38:36 +1000

    added property getters

--- src/trivialscan/evaluations/__init__.py
+++ src/trivialscan/evaluations/__init__.py
@@ -4,19 +4,31 @@ from ..transport import Transport
 
 class BaseEvaluationTask:
     _transport: Transport
-    _state: TransportState
     _metadata: dict
     substitution_metadata: dict
 
     def __init__(
         self,
         transport: Transport,
-        state: TransportState,
         metadata: dict,
         configuration: dict,
     ) -> None:
         self._transport = transport
-        self._state = state
         self._metadata = metadata
         self._configuration = configuration
         self.substitution_metadata = {}
+
+
+    @property
+    def transport(self) -> Transport:
+        return self._transport
+
+
+    @property
+    def state(self) -> TransportState:
+        return self._transport.get_state()
+
+
+    @property
+    def metadata(self) -> dict:
+        return self._metadata
