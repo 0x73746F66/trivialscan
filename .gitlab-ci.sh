9b2f82ba6efc8a213578c02d58fbe9522439a75d
chris <chris@trivialsec.com>
2022-09-06 13:29:50 +1000

    fix: tag gitlab-org

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -1,4 +1,5 @@
 (
-    pip install --disable-pip-version-check -U pip
-    pip install -r requirements-ci.txt
+    apt install python3-pip
+    pip install --user --disable-pip-version-check -U pip
+    pip install --user -r requirements-ci.txt
 )
