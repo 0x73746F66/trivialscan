b81e53b5a10e1349087a8cf71a2e004326426378
chris <chris@trivialsec.com>
2022-10-25 00:21:21 +1100

    feat: add cert expiring rule, all rules given consistent scores

---
+++ src/trivialscan/evaluations/certificate/certification_expiring.py
@@ -0,0 +1,17 @@
+from typing import Union
+from datetime import datetime, timedelta
+from ...transport import TLSTransport
+from ...certificate import BaseCertificate
+from .. import BaseEvaluationTask
+
+
+class EvaluationTask(BaseEvaluationTask):
+    def __init__(  # pylint: disable=useless-super-delegation
+        self, transport: TLSTransport, metadata: dict, config: dict
+    ) -> None:
+        super().__init__(transport, metadata, config)
+
+    def evaluate(self, certificate: BaseCertificate) -> Union[bool, None]:
+        self.substitution_metadata["expiry_status"] = certificate.expiry_status
+        risk = datetime.utcnow() + timedelta(days=3)
+        return certificate.not_after < risk
